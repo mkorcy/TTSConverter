ad216e71cfd7518fbf23937e74f6fc320d5bec83
agolin95 <agolin95@gmail.com>
2015-07-30 14:59:35 -0400

    Conversion working

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,3 +1,7 @@
+require 'srt'
+require 'nokogiri'
+require './template.rb'
+
 class ConversionsController < ApplicationController
 
   def show
@@ -9,9 +13,35 @@ class ConversionsController < ApplicationController
   end
 
   def create
-  	@conversion = Conversion.new(conversion_params)
+    
+    @conversion = Conversion.new(conversion_params)
+
+    #read contents of uploaded file
+    file_contents = params[:conversion][:file].tempfile.read
+
+    #write contents of uploaded file to rails app default srt file
+    #this write command overwrites any content already in default file
+    File.open("defaultsrt.srt", "wb+") do |f|
+      f.write(file_contents)
+    end
+
+    #execute conversion script from default srt file to default xml file
+    %x(ruby convert_srt_to_tei.rb defaultsrt.srt defaultxml.xml)
+
+    ##########
+    #add in author and all that jazz in here
+
+
+    ##########
+
+    #read contents of converted file
+    converted_contents = File.read("defaultxml.xml")
+
   	if @conversion.save
-  		redirect_to @conversion
+      #deliver converted contents to file download
+      send_data converted_contents, :filename => 'writeto.xml'
+  		#render "static_pages/home"
+      #redirect_to @conversion
   	else
   		render 'new'
   	end
@@ -19,10 +49,18 @@ class ConversionsController < ApplicationController
 
   private
 
+    def trialmethod(name)
+      name = 'Alexander'
+    end
+
 	  def conversion_params
 	  	params.require(:conversion).permit(:title, :author, :pid, 
 	  									   :recording, :description, :file)
 	  end
+
+    def edit
+      @conversion = Conversion.find(params[:id])
+    end
 
 end
 
